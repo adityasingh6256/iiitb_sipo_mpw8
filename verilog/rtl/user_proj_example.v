bbbfc3861894f7b893d013309b727d65e457b414
ADITYA SINGH <110079790+adityasingh6256@users.noreply.github.com>
2022-12-09 16:22:27 +0530

    Update user_proj_example.v

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -70,96 +70,52 @@ module user_proj_example #(
 );
     wire clk;
     wire rst;
+    wire [3:0] q, qbar;
+    wire d;
 
     wire [`MPRJ_IO_PADS-1:0] io_in;
     wire [`MPRJ_IO_PADS-1:0] io_out;
     wire [`MPRJ_IO_PADS-1:0] io_oeb;
 
-    wire [31:0] rdata; 
-    wire [31:0] wdata;
-    wire [BITS-1:0] count;
-
-    wire valid;
-    wire [3:0] wstrb;
-    wire [31:0] la_write;
-
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = rdata;
-    assign wdata = wbs_dat_i;
+   
+    
 
     // IO
-    assign io_out = count;
-    assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
+    assign clk = wb_clk_i;
+    assign rst = wb_rst_i;
+    assign d = io_in[35];
+    assign io_out[37:30] = {q,qbar};
+    assign io_oeb = 0;
 
     // IRQ
     assign irq = 3'b000;	// Unused
 
-    // LA
-    assign la_data_out = {{(127-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:32] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
-
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:32]),
-        .count(count)
-    );
+    iiitb_sipo instance (d,rst,clk,q,qbar);
+   
 
 endmodule
 
-module counter #(
-    parameter BITS = 32
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output ready,
-    output [BITS-1:0] rdata,
-    output [BITS-1:0] count
-);
-    reg ready;
-    reg [BITS-1:0] count;
-    reg [BITS-1:0] rdata;
-
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-            ready <= 0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-                if (wstrb[2]) count[23:16] <= wdata[23:16];
-                if (wstrb[3]) count[31:24] <= wdata[31:24];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
+module dff(d,rst,clk,q);
+    input d,clk,rst;
+    output reg q;
+    always @ (posedge clk)
+    begin
+    if(~rst)
+    q<=0;
+    else
+    q<=d;
     end
+endmodule 
 
+module iiitb_sipo(d, rst, clk, q, qbar);
+    input d,clk,rst;
+    output [3:0]q;
+    output [3:0]qbar;
+    dff aa(d, rst, clk, q[3]);
+    dff bb(q[3], rst, clk, q[2]);
+    dff cc(q[2], rst, clk, q[1]);
+    dff dd(q[1], rst, clk, q[0]);
+    assign qbar = ~q;
 endmodule
+
 `default_nettype wire
